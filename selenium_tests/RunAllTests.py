081926a9fbb2629ef0a4ca1c347c07fde66466c3
bacowan <bacowan@ualberta.ca>
2021-03-23 23:00:50 -0300

    writing filter tests and adding comments to selenium tests

--- selenium_tests/RunAllTests.py
+++ selenium_tests/RunAllTests.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
 import time
 
 class UITests(unittest.TestCase):
@@ -15,44 +16,56 @@ class UITests(unittest.TestCase):
     
     def test_with_valid_data(self):
         startWithArgs(self.driver, 'valid_data')
-        firstPoi = self.driver.find_element_by_class_name("poi-list").find_elements_by_tag_name("li")[0]
-        secondPoi = self.driver.find_element_by_class_name("poi-list").find_elements_by_tag_name("li")[1]
-        thirdPoi = self.driver.find_element_by_class_name("poi-list").find_elements_by_tag_name("li")[2]
+        poiList = self.driver.find_element_by_class_name("poi-list")
+        firstPoi = poiList.find_elements_by_tag_name("li")[0]
+        secondPoi = poiList.find_elements_by_tag_name("li")[1]
+        thirdPoi = poiList.find_elements_by_tag_name("li")[2]
         previousButton = self.driver.find_element_by_class_name("navigation-buttons").find_elements_by_tag_name("button")[0]
         nextButton = self.driver.find_element_by_class_name("navigation-buttons").find_elements_by_tag_name("button")[1]
-        
+        filterSelect = Select(self.driver.find_element_by_tag_name("select"))
+
         # Initial state
-        self.assertFalse('selected-item' in firstPoi.get_attribute("class"))
-        self.assertFalse('selected-item' in secondPoi.get_attribute("class"))
-        self.assertEqual(0, len(self.driver.find_elements_by_class_name("mapboxgl-popup")))
+        self.assertFalse('selected-item' in firstPoi.get_attribute("class"), "Item is initially selected when it shouldn't be")
+        self.assertFalse('selected-item' in secondPoi.get_attribute("class"), "Item is initially selected when it shouldn't be")
+        self.assertEqual(0, len(self.driver.find_elements_by_class_name("mapboxgl-popup")), "Popup is initially visible when it shouldn't be")
 
         # popup and recolouration when you click a poi
         firstPoi.click()
-        self.assertTrue('selected-item' in firstPoi.get_attribute("class"))
-        self.assertFalse('selected-item' in secondPoi.get_attribute("class"))
+        self.assertTrue('selected-item' in firstPoi.get_attribute("class"), "Clicking on the first item in the list didn't highlight it")
+        self.assertFalse('selected-item' in secondPoi.get_attribute("class"), "Clicking on the first item in the list highlighted the second one")
         popups = self.driver.find_elements_by_class_name("mapboxgl-popup")
-        self.assertEqual(1, len(popups))
-        self.assertEqual("a", popups[0].find_element_by_tag_name("strong").get_attribute("innerHTML"))
+        self.assertEqual(1, len(popups), "0 or more than 1 popups appeard when an item in the list was clicked")
+        self.assertTrue(popups[0].find_element_by_tag_name("strong").get_attribute("innerHTML").startswith("a:"), "The wrong popup appeared when an item in the list was clicked")
 
         # recolour and new popup when you click another poi
         secondPoi.click()
-        self.assertFalse('selected-item' in firstPoi.get_attribute("class"))
-        self.assertTrue('selected-item' in secondPoi.get_attribute("class"))
+        self.assertFalse('selected-item' in firstPoi.get_attribute("class"), "Clicking on the second item in the list didn't unselect the first one")
+        self.assertTrue('selected-item' in secondPoi.get_attribute("class"), "Clicking on the second item in the list didn't highlight it")
         popups = self.driver.find_elements_by_class_name("mapboxgl-popup")
-        self.assertEqual(1, len(popups))
-        self.assertEqual("b", popups[0].find_element_by_tag_name("strong").get_attribute("innerHTML"))
+        self.assertEqual(1, len(popups), "0 or more than 1 popups appeard when an item in the list was clicked")
+        self.assertTrue(popups[0].find_element_by_tag_name("strong").get_attribute("innerHTML").startswith("b:"), "The wrong popup appeared when an item in the list was clicked")
 
         # next button goes next
         nextButton.click()
-        self.assertFalse('selected-item' in firstPoi.get_attribute("class"))
-        self.assertFalse('selected-item' in secondPoi.get_attribute("class"))
-        self.assertTrue('selected-item' in thirdPoi.get_attribute("class"))
+        self.assertFalse('selected-item' in firstPoi.get_attribute("class"), "Clicking the next button highlighted the previous item")
+        self.assertFalse('selected-item' in secondPoi.get_attribute("class"), "Clicking the next button kept the current item highlighted")
+        self.assertTrue('selected-item' in thirdPoi.get_attribute("class"), "Clicking the next button didn't highlight the next item")
 
         # previous button goes back again
         previousButton.click()
-        self.assertFalse('selected-item' in firstPoi.get_attribute("class"))
-        self.assertTrue('selected-item' in secondPoi.get_attribute("class"))
-        self.assertFalse('selected-item' in thirdPoi.get_attribute("class"))
+        self.assertFalse('selected-item' in firstPoi.get_attribute("class"), "Clicking the previous button highlighted the next item")
+        self.assertTrue('selected-item' in secondPoi.get_attribute("class"), "Clicking the previous button didn't highlight the previous item")
+        self.assertFalse('selected-item' in thirdPoi.get_attribute("class"), "Clicking the previous button kept the current item highlighted")
+
+        # The filter filters things
+        filterSelect.select_by_value("StartStop")
+        self.assertEqual(2, len(poiList.find_elements_by_tag_name("li")), "The incorrect number of items remained visible when a filter was selected")
+        self.assertEqual("a", poiList.find_elements_by_tag_name("li")[0].get_attribute("innerHTML"), "An item with the selected filter was hidden")
+        self.assertEqual("c", poiList.find_elements_by_tag_name("li")[1].get_attribute("innerHTML"), "An item with the selected filter was hidden")
+
+        # Resetting the filter brings them back
+        filterSelect.select_by_value("")
+        self.assertEqual(3, len(poiList.find_elements_by_tag_name("li")), "Clearing the filter didn't reset the visible items")
 
     def test_with_blank_data(self):
         startWithArgs(self.driver, 'blank_data')
--- selenium_tests/testData/valid_data.js
+++ selenium_tests/testData/valid_data.js
@@ -3,18 +3,24 @@
         "lat": 45.523241975045636,
         "lon": 141.93649435423438,
         "title": "a",
-        "description": "a"
+        "description": "a",
+        "plannedArrivalDate": "August 1 2022",
+        "tags": ["StartStop", "Onsen"]
     },
     {
         "lat": 35.638597097766834,
         "lon": 139.71881476836123,
         "title": "b",
-        "description": "b"
+        "description": "b",
+        "plannedArrivalDate": "August 2 2022",
+        "tags": ["Onsen"]
     },
     {
         "lat": 30.997005309529722,
         "lon": 130.65926019195493,
         "title": "c",
-        "description": "c"
+        "description": "c",
+        "plannedArrivalDate": "August 3 2022",
+        "tags": ["StartStop"]
     }
 ]
